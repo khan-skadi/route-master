fc60bc66bd121e4308a3224981843699b06cacb0
khan-skadi <kartalov.pt@gmail.com>
2020-02-02 18:28:14 +0100

    fix

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import DriverSelectOptions from "../drivers/DriverSelectOptions";
 import { addLog } from "../../store/actions/logActions";
+import DriverSelectOptions from "../drivers/DriverSelectOptions";
+import PropTypes from "prop-types";
 import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = ({ addLog }) => {
@@ -14,17 +14,6 @@ const AddLogModal = ({ addLog }) => {
   const [attention, setAttention] = useState(false);
   const [progress, setProgress] = useState(false);
   const [driver, setDriver] = useState("");
-
-  // useEffect(() => {
-  //   if (currentRoute) {
-  //     setLocationFrom(currentRoute.locationFrom);
-  //     setLocationTo(currentRoute.locationTo);
-  //     setDistance(currentRoute.distance);
-  //     setPostedOn(currentRoute.postedOn);
-  //     setPostedBy(currentRoute.postedBy);
-  //     setId(currentRoute.id);
-  //   }
-  // }, [currentRoute]);
 
   const onSubmit = () => {
     const newRoute = {
@@ -42,136 +31,153 @@ const AddLogModal = ({ addLog }) => {
     addLog(newRoute);
 
     M.toast({ html: "Route added" });
+
+    //  Clear Fields
+    setLocationFrom("");
+    setLocationTo("");
+    setDistance("");
+    setPostedOn("");
+    setPostedBy("");
+    setAttention(false);
+    setProgress(false);
+    setDriver("");
   };
 
   return (
     <div id="add-log-modal" className="modal" style={modalStyle}>
       <div className="modal-content">
-        <h4>Submit Route</h4>
-
         <div className="row">
-          <div className="input-field">
-            <input
-              type="text"
-              name="locationFrom"
-              value={locationFrom || ""}
-              onChange={e => setLocationFrom(e.target.value)}
-            />
-            <label htmlFor="locationFrom" className="active">
-              Location From
-            </label>
+          <div className="col s12">
+            <h4>Enter Route</h4>
+            <br />
           </div>
         </div>
 
         <div className="row">
-          <div className="input-field">
-            <input
-              type="text"
-              name="locationTo"
-              value={locationTo || ""}
-              onChange={e => setLocationTo(e.target.value)}
-            />
-            <label htmlFor="locationTo" className="active">
-              Location To
-            </label>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="input-field">
-            <select
-              name="driver"
-              value={driver}
-              className="browser-default"
-              onChange={e => setDriver(e.target.value)}
-            >
-              <option value="" disabled>
-                Driver
-              </option>
-              <DriverSelectOptions />
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field">
-          <input
-            type="text"
-            name="distance"
-            value={distance || ""}
-            onChange={e => setDistance(e.target.value)}
-          />
-          <label htmlFor="distance" className="active">
-            Distance
-          </label>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field">
-          <input
-            type="text"
-            name="postedOn"
-            value={postedOn || ""}
-            onChange={e => setPostedOn(e.target.value)}
-          />
-          <label htmlFor="postedOn" className="active">
-            Posted On
-          </label>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field">
-          <input
-            type="text"
-            name="postedBy"
-            value={postedBy || ""}
-            onChange={e => setPostedBy(e.target.value)}
-          />
-          <label htmlFor="postedBy" className="active">
-            Posted By
-          </label>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field">
-          <p>
-            <label>
+          <div className="col s12">
+            <div className="input-field">
               <input
-                type="checkbox"
-                className="filled-in"
-                checked={attention}
-                value={attention}
-                onChange={e => setAttention(!attention)}
+                type="text"
+                name="locationFrom"
+                value={locationFrom}
+                onChange={e => setLocationFrom(e.target.value)}
               />
-              <span>Important - Time Sensitive</span>
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                type="checkbox"
-                className="filled-in"
-                checked={progress}
-                value={progress}
-                onChange={e => setProgress(!progress)}
-              />
-              <span>In Progress</span>
-            </label>
-          </p>
+              <label htmlFor="locationFrom" className="active">
+                Location From
+              </label>
+            </div>
+          </div>
 
-          <div className="modal-footer">
-            <a
-              href="#!"
-              onClick={onSubmit}
-              className="modal-close waves-effect blue darken-3 btn"
-            >
-              Add Route
-              <i className="material-icons right">send</i>
-            </a>
+          <div className="col s12">
+            <div className="input-field">
+              <input
+                type="text"
+                name="locationTo"
+                value={locationTo}
+                onChange={e => setLocationTo(e.target.value)}
+              />
+              <label htmlFor="locationTo" className="active">
+                Location To
+              </label>
+            </div>
+          </div>
+
+          <div className="col s12">
+            <div className="input-field">
+              <select
+                name="driver"
+                value={driver}
+                className="browser-default"
+                onChange={e => setDriver(e.target.value)}
+              >
+                <option value="" disabled>
+                  Select Driver
+                </option>
+                <DriverSelectOptions />
+              </select>
+            </div>
+          </div>
+
+          <div className="col s12">
+            <div className="input-field">
+              <input
+                type="text"
+                name="distance"
+                value={distance}
+                onChange={e => setDistance(e.target.value)}
+              />
+              <label htmlFor="distance" className="active">
+                Distance
+              </label>
+            </div>
+          </div>
+
+          <div className="col s12">
+            <div className="input-field">
+              <input
+                type="text"
+                name="postedOn"
+                value={postedOn}
+                onChange={e => setPostedOn(e.target.value)}
+              />
+              <label htmlFor="postedOn" className="active">
+                Posted On
+              </label>
+            </div>
+          </div>
+
+          <div className="col s12">
+            <div className="input-field">
+              <input
+                type="text"
+                name="postedBy"
+                value={postedBy}
+                onChange={e => setPostedBy(e.target.value)}
+              />
+              <label htmlFor="postedBy" className="active">
+                Posted By
+              </label>
+            </div>
+          </div>
+
+          <div className="col s12">
+            <div className="input-field">
+              <p>
+                <label>
+                  <input
+                    type="checkbox"
+                    className="filled-in"
+                    checked={attention}
+                    value={attention}
+                    onChange={e => setAttention(!attention)}
+                  />
+                  <span>Important - Time Sensitive</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <input
+                    type="checkbox"
+                    className="filled-in"
+                    checked={progress}
+                    value={progress}
+                    onChange={e => setProgress(!progress)}
+                  />
+                  <span>In Progress</span>
+                </label>
+              </p>
+
+              <div className="modal-footer">
+                <a
+                  href="#!"
+                  onClick={onSubmit}
+                  className="modal-close waves-effect blue darken-3 btn"
+                >
+                  Add Route
+                  <i className="material-icons right">send</i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -180,8 +186,8 @@ const AddLogModal = ({ addLog }) => {
 };
 
 const modalStyle = {
-  width: "75%",
-  height: "75%"
+  width: "45%",
+  height: "100%"
 };
 
 AddLogModal.propTypes = {
@@ -189,122 +195,3 @@ AddLogModal.propTypes = {
 };
 
 export default connect(null, { addLog })(AddLogModal);
-
-// const AddLogModal = ({ addLog }) => {
-//   const [message, setMessage] = useState("");
-//   const [driver, setDriver] = useState("");
-//   const [attention, setAttention] = useState(false);
-//   const [progress, setProgress] = useState(false);
-
-//   const onSubmit = () => {
-//     if (message === "" || driver === "") {
-//       M.toast({ html: "Please describe route and enter driver" });
-//     } else {
-//       const newRoute = {
-//         message,
-//         attention,
-//         progress,
-//         driver,
-//         date: new Date()
-//       };
-
-//       addLog(newRoute);
-
-//       M.toast({ html: `Route added by ${driver}` });
-
-//       // Clear Fields
-//       setMessage("");
-//       setDriver("");
-//       setAttention(false);
-//       setProgress(false);
-//     }
-//   };
-
-//   return (
-//     <div id="add-log-modal" className="modal" style={modalStyle}>
-//       <div className="modal-content">
-//         <h4>Submit Route</h4>
-//         <div className="row">
-//           <div className="input-field">
-//             <input
-//               type="text"
-//               name="message"
-//               value={message}
-//               onChange={e => setMessage(e.target.value)}
-//             />
-//             <label htmlFor="message" className="active">
-//               Route info
-//             </label>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="input-field">
-//             <select
-//               name="driver"
-//               value={driver}
-//               className="browser-default"
-//               onChange={e => setDriver(e.target.value)}
-//             >
-//               <option value="" disabled>
-//                 Driver
-//               </option>
-//               <DriverSelectOptions />
-//             </select>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="input-field">
-//             <p>
-//               <label>
-//                 <input
-//                   type="checkbox"
-//                   className="filled-in"
-//                   checked={attention}
-//                   value={attention}
-//                   onChange={e => setAttention(!attention)}
-//                 />
-//                 <span>Important - Time Sensitive</span>
-//               </label>
-//             </p>
-//             <p>
-//               <label>
-//                 <input
-//                   type="checkbox"
-//                   className="filled-in"
-//                   checked={progress}
-//                   value={progress}
-//                   onChange={e => setProgress(!progress)}
-//                 />
-//                 <span>In Progress</span>
-//               </label>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="modal-footer">
-//         <a
-//           href="#!"
-//           onClick={onSubmit}
-//           className="modal-close waves-effect blue darken-3 btn"
-//         >
-//           Submit
-//           <i className="material-icons right">send</i>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// AddLogModal.propTypes = {
-//   addLog: PropTypes.func.isRequired
-// };
-
-// const modalStyle = {
-//   width: "75%",
-//   height: "75%"
-// };
-
-// export default connect(null, { addLog })(AddLogModal);
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -42,6 +42,15 @@ const EditLogModal = ({ current, updateLog }) => {
     updateLog(updRoute);
 
     M.toast({ html: "Route edited" });
+
+    setLocationFrom("");
+    setLocationTo("");
+    setDistance("");
+    setPostedOn("");
+    setPostedBy("");
+    setAttention(false);
+    setProgress(false);
+    setDriver("");
   };
 
   return (
@@ -135,17 +144,6 @@ const EditLogModal = ({ current, updateLog }) => {
           </label>
         </div>
       </div>
-      {/* 
-      <div className="modal-footer">
-        <a
-          href="#!"
-          onClick={onSubmit}
-          className="modal-close waves-effect blue darken-3 btn"
-        >
-          Add Route
-          <i className="material-icons right">send</i>
-        </a>
-      </div> */}
 
       <div className="row">
         <div className="input-field">
@@ -204,133 +202,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, { updateLog })(EditLogModal);
-
-// const EditLogModal = ({ current, updateLog }) => {
-//   const [message, setMessage] = useState("");
-//   const [driver, setDriver] = useState("");
-//   const [attention, setAttention] = useState(false);
-//   const [progress, setProgress] = useState(false);
-
-//   useEffect(() => {
-//     if (current) {
-//       setMessage(current.message);
-//       setAttention(current.attention);
-//       setDriver(current.driver);
-//       setProgress(current.progress);
-//     }
-//   }, [current]);
-
-//   const onSubmit = () => {
-//     if (message === "" || driver === "") {
-//       M.toast({ html: "Please submit a message and a driver" });
-//     } else {
-//       const updLog = {
-//         id: current.id,
-//         message,
-//         attention,
-//         progress,
-//         driver,
-//         date: new Date()
-//       };
-
-//       updateLog(updLog);
-//       M.toast({ html: `Route updated by ${driver}` });
-
-//       // Clear Fields
-//       setMessage("");
-//       setDriver("");
-//       setAttention(false);
-//       setProgress(false);
-//     }
-//   };
-
-//   return (
-//     <div id="edit-log-modal" className="modal" style={modalStyle}>
-//       <div className="modal-content">
-//         <h4>Edit Route</h4>
-//         <div className="row">
-//           <div className="input-field">
-//             <input
-//               type="text"
-//               name="message"
-//               value={message || ""}
-//               onChange={e => setMessage(e.target.value)}
-//             />
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="input-field">
-//             <select
-//               name="driver"
-//               value={driver || ""}
-//               className="browser-default"
-//               onChange={e => setDriver(e.target.value)}
-//             >
-//               <option value="" disabled>
-//                 Select Driver
-//               </option>
-//               <DriverSelectOptions />
-//             </select>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="input-field">
-//             <p>
-//               <label>
-//                 <input
-//                   type="checkbox"
-//                   className="filled-in"
-//                   checked={attention}
-//                   value={attention}
-//                   onChange={e => setAttention(!attention)}
-//                 />
-//                 <span>Needs Attention</span>
-//               </label>
-//             </p>
-//             <p>
-//               <label>
-//                 <input
-//                   type="checkbox"
-//                   className="filled-in"
-//                   checked={progress}
-//                   value={progress}
-//                   onChange={e => setProgress(!progress)}
-//                 />
-//                 <span>In Progress</span>
-//               </label>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="modal-footer">
-//         <a
-//           href="#!"
-//           onClick={onSubmit}
-//           className="modal-close waves-effect blue darken-3 btn"
-//         >
-//           Submit
-//           <i className="material-icons right">send</i>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const modalStyle = {
-//   width: "75%",
-//   height: "75%"
-// };
-
-// EditLogModal.propTypes = {
-//   current: PropTypes.object,
-//   updateLog: PropTypes.func.isRequired
-// };
-
-// const mapStateToProps = state => ({
-//   current: state.log.current
-// });
-
-// export default connect(mapStateToProps, { updateLog })(EditLogModal);
