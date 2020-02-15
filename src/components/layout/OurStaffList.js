6b1bd95e17674c5f504cec7c26381e9683226f0a
Skadi <kartalov.pt@gmail.com>
2020-02-15 17:17:54 +0100

    fix admin panel responsiveness on large screen

--- src/components/layout/OurStaffList.js
+++ src/components/layout/OurStaffList.js
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const OurStaffList = ({ driver }) => {
+  const truncate = string => {
+    return string.length > 8 ? string.substring(0, 7) + "..." : string;
+  };
+
   return (
     <ul className="list-inline">
       <li>
         <div className="col l4 s12 m6">
-          <div className="card medium">
+          <div className="card">
             <div className="card-image waves-effect waves-block waves-light">
               <img src={driver.url} className="activator" alt="" />
             </div>
             <div className="card-content">
               <span className="card-title activator grey-text text-darken-4">
-                {driver.firstName}
+                {truncate(driver.firstName)}
                 <i className="material-icons right">more_vert</i>
               </span>
               <span className="blue-text text-darken-2">HourlyRate:</span>
@@ -21,6 +25,7 @@ const OurStaffList = ({ driver }) => {
               <span className="blue-text text-darken-2">License:</span>
               <p>{driver.license}</p>
             </div>
+            {/* Open Card */}
             <div className="card-reveal">
               <span className="card-title grey-text text-darken-4">
                 {driver.firstName} {driver.lastName}
@@ -38,7 +43,9 @@ const OurStaffList = ({ driver }) => {
             </div>
             <div className="card-action">
               <Link to={"/drivers/" + driver.id}>
-                <button className="btn btn-primary">Profile</button>
+                <button className="btn btn-primary blue darken-2">
+                  Profile
+                </button>
               </Link>
             </div>
           </div>
